300733416d5df7bc653c6f52a6fadf2f57a7d399
monicafu <31578677+monicafu@users.noreply.github.com>
2018-03-29 00:14:56 -0700

    delete commits

--- exams/exam02/part02/guessWord.js
+++ exams/exam02/part02/guessWord.js
@@ -5,12 +5,10 @@ const compare = require ('./compareLetter');
 const guessWord = (wordList,guessed,matched) => {
     let copyList = wordList;
     let newGuess = '';
-    // console.log('------------------------');
-    // console.log("old matched: "+guessed, " , "+matched);
     let temp = [];
     if (guessed === undefined || matched === 0){
         newGuess = random (copyList);
-    }else if (matched !== 0){
+    }else {
         const guessedIdx = copyList.indexOf(guessed);
         copyList.splice(guessedIdx,1);
         copyList.forEach((element) =>{
@@ -20,11 +18,8 @@ const guessWord = (wordList,guessed,matched) => {
             }
         });
         copyList = temp;
-        // console.log('copyList len: '+temp.length);
         newGuess = random(copyList);
     }
-    // console.log(`new guess :${newGuess}`);
-    // console.log('------------------------');
     return newGuess;
 };
 
